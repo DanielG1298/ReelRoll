63cc32d93b79d88593fa3c4a3708fe49459bc8e3
DanielG1298 <glaser.daniel1204@gmail.com>
2025-12-28 20:02:23 -0600

    finished review route and it rready for review added a func to update reviews to query

--- Backend/api/reviewRouter.js
+++ Backend/api/reviewRouter.js
@@ -1,5 +1,80 @@
 import express from 'express';
 const reviewRouter = express.Router();
+import {createReview, getReviewsByMovieId, getReviewsByUserId,updateReview, deleteReview} from '../db/queries/reviews.js';
+import requireUser from '../middleware/requireUser.js';
+
 export default reviewRouter;
 
+// requires users too be logged in to create, update, delete reviews
+reviewRouter.use(requireUser);
 
+// create a review 
+reviewRouter.post('/', async( req, res, next) =>{
+    try{
+        const userId = req.user.id;
+        
+        const { movieId, rating, comment } = req.body;
+
+        const review = await createReview({ userId, movieId, rating, comment});
+        res.status(201).json(review);
+    }catch(err){
+        next(err);
+    }
+});
+
+// get reviews by movie id 
+reviewRouter.get('/movie/:movieId', async(req,res,next) =>{
+    try{
+       const movieId = req.params.movieId; 
+
+       const reviews = await getReviewsByMovieId(movieId);
+       
+       res.status(200).json(reviews);
+    }catch(err){
+        next(err);
+    }
+});
+
+
+//get reviews by user id 
+reviewRouter.get('/reviews', async(req, res, next) =>{
+    try{
+        const userId = req.params.userId;
+
+        const reviews = await getReviewsByUserId(userId);
+        res.status(200).json(reviews)
+    }catch(err){
+        next(err);
+    }
+});
+
+// update review
+reviewRouter.put('/:reviewId', async(req,res,next) =>{
+    try{
+        const reviewId = req.params.reviewId;
+
+        const { rating, comment } = req.body;
+
+        const review = await updateReview({ reviewId, rating, comment });
+
+        if (!review) return res.status(404).send('Review not found');
+
+        res.status(200).json(review);
+    }catch(err){
+        next(err);
+    }
+});
+
+// delete review
+reviewRouter.delete('/:reviewId', async(req,res,next) =>{
+    try{
+        const reviewId = req.params.reviewId;
+
+        const review = await deleteReview(reviewId);
+
+        if(!review) return res.status(404).send('Review not found');
+        res.json(review);
+    }catch(err){
+        next(err);
+    }
+});
--- Backend/db/queries/reviews.js
+++ Backend/db/queries/reviews.js
@@ -1,6 +1,6 @@
 import db from '../client.js';
-
-export async function createReview({ userId, movieId, userId, rating, comment}){
+// add review to table 
+export async function createReview({ userId, movieId, rating, comment}){
     const sql = `
     INSERT INTO reviews (user_id, movie_id, rating, comment)
     VALUES ($1, $2, $3, $4)
@@ -8,18 +8,33 @@ export async function createReview({ userId, movieId, userId, rating, comment}){
     const { rows: [review] } = await db.query(sql, [userId, movieId, rating, comment]);
     return review;
 }
+// get reviews by movie id
 export async function getReviewsByMovieId(movieId){
     const sql = `
     SELECT * FROM reviews WHERE movie_id = $1`;
     const { rows: reviews } = await db.query(sql, [movieId]);
     return reviews;
 }
+// get reviews by user id 
  export async function getReviewsByUserId(userId){
     const sql = `
     SELECT * FROM reviews WHERE user_id = $1`;
     const { rows: reviews } = await db.query(sql, [userId]);
     return reviews;
 }
+
+// update reviews viw userid, movieid
+export async function updateReview({ reviewId, rating, comment}){
+    const sql = `
+    UPDATE reviews
+    SET rating = $2, comment = $3
+    WHERE id = $1
+    RETURNING *;`;
+    const { rows: [review] } = await db.query(sql, [reviewId, rating, comment]);
+    return review;
+    
+}
+//delete review by id 
 export async function deleteReview(reviewId){
     const sql = `
     DELETE FROM reviews WHERE id = $1
